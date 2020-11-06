8ec4b295ead1b6c9b337121a7fde2773e2049b3e
Dalton Okechukwu <okechukwu@whitewhale.ai>
2020-11-05 20:10:06 -0700

    initial setup incl styled-components and contexts for AppBar

---
+++ src/App/AppBar.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { AppContext } from './AppProvider';
+
+const Bar = styled.div`
+    display: grid;
+    grid-template-columns: 180px auto 100px 100px;
+    margin-bottom: 40px;
+`
+
+const Logo = styled.div`
+    font-size: 1.5em;
+`
+
+const ControlButtonElem = styled.div`
+    cursor: pointer;
+    ${props => props.active && css`
+        color: #0cff00;
+    `}
+`
+const toProperCase = (lower) => {
+    return lower.charAt(0).toUpperCase() + lower.substr(1);
+}
+
+const ControlButton = ({name}) => {
+    return (
+        <AppContext.Consumer>
+            {({page, setPage}) => (
+                <ControlButtonElem active={page === name} onClick={() => setPage(name)} >
+                    {toProperCase(name)} 
+                </ControlButtonElem>
+            )
+            }
+        </AppContext.Consumer>
+    )
+}
+
+const AppBar = () => {
+    return ( 
+    <Bar>
+        <Logo>CryptoDash</Logo>
+        <div/>
+        <ControlButton active name="dashboard"/>
+        <ControlButton name="settings"/>
+    </Bar> 
+    );
+}
+ 
+export default AppBar;
---
+++ src/App/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import WelcomeMessage from "./WelcomeMessage";
+import "./App.css";
+import AppLayout from "./AppLayout";
+import AppBar from "./AppBar";
+import { AppProvider } from "./AppProvider";
+
+class App extends Component {
+  render() {
+    return (
+      <AppLayout>
+        <AppProvider>
+          <AppBar />
+          <WelcomeMessage />
+        </AppProvider>
+      </AppLayout>
+    );
+  }
+}
+
+export default App;
