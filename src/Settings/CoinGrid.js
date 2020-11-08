2002368dc72c2f96536e37423683a0c05024dd34
Dalton Okechukwu <okechukwu@whitewhale.ai>
2020-11-08 11:08:02 -0700

    made coinGrid responsive

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -5,13 +5,13 @@ import CoinTile from './CoinTile';
 
 export const CoinGridStyled = styled.div`
     display: grid;
-    grid-template-columns: repeat(5, 1fr);
+    grid-template-columns: repeat(auto-fill, minmax(120px, 1fr));
     grid-gap: 15px;
     margin-top: 20px;
 `
 
 const getCoinsToDisplay = (coinList, topSection, favorites) => {
-    return topSection ? favorites : Object.keys(coinList).slice(0, 100);
+    return topSection ? favorites : Object.keys(coinList).slice(0, 120);
 }
 const CoinGrid = ({topSection}) => {
     return (
